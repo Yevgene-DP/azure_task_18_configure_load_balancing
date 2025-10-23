669117a90062637cabb435f2d16a495fcbfc1edd
Yevgene-DP <ebalalay@gmail.com>
2025-10-23 23:36:43 +0300

    Create load balancer

--- task.ps1
+++ task.ps1
@@ -1,116 +1,209 @@
-$location = "uksouth"
-$resourceGroupName = "mate-azure-task-18"
+# Parameters
+$ResourceGroupName = "mate-azure-task-18"
+$Location = "East US"
+$VNetName = "web-vnet"
+$WebSubnetName = "web"
+$JumpboxSubnetName = "jumpbox"
+$SshKeyName = "webserver-ssh-key"
 
-$virtualNetworkName = "todoapp"
-$vnetAddressPrefix = "10.20.30.0/24"
-$webSubnetName = "webservers"
-$webSubnetIpRange = "10.20.30.0/26"
-$mngSubnetName = "management"
-$mngSubnetIpRange = "10.20.30.128/26"
-
-$sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
-
-$vmImage = "Ubuntu2204"
-$vmSize = "Standard_B1s"
-$webVmName = "webserver"
-$jumpboxVmName = "jumpbox"
-$dnsLabel = "matetask" + (Get-Random -Count 1)
-
-$privateDnsZoneName = "or.nottodo"
-
-$lbName = "loadbalancer"
-$lbIpAddress = "10.20.30.62"
-
-
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
-
-Write-Host "Creating web network security group..."
-$webHttpRule = New-AzNetworkSecurityRuleConfig -Name "web" -Description "Allow HTTP" `
-   -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
-   Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
-$webNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-   $webSubnetName -SecurityRules $webHttpRule
-
-Write-Host "Creating mngSubnet network security group..."
-$mngSshRule = New-AzNetworkSecurityRuleConfig -Name "ssh" -Description "Allow SSH" `
-   -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix `
-   Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22
-$mngNsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroupName -Location $location -Name `
-   $mngSubnetName -SecurityRules $mngSshRule
-
-Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $webNsg
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange -NetworkSecurityGroup $mngNsg
-$virtualNetwork = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$mngSubnet
-
-Write-Host "Creating a SSH key resource ..."
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
-
-Write-Host "Creating a web server VM ..."
-
-for (($zone = 1); ($zone -le 2); ($zone++) ) {
-   $vmName = "$webVmName-$zone"
-   New-AzVm `
-   -ResourceGroupName $resourceGroupName `
-   -Name $vmName `
-   -Location $location `
-   -image $vmImage `
-   -size $vmSize `
-   -SubnetName $webSubnetName `
-   -VirtualNetworkName $virtualNetworkName `
-   -SshKeyName $sshKeyName 
-   $Params = @{
-      ResourceGroupName  = $resourceGroupName
-      VMName             = $vmName
-      Name               = 'CustomScript'
-      Publisher          = 'Microsoft.Azure.Extensions'
-      ExtensionType      = 'CustomScript'
-      TypeHandlerVersion = '2.1'
-      Settings          = @{fileUris = @('https://raw.githubusercontent.com/mate-academy/azure_task_18_configure_load_balancing/main/install-app.sh'); commandToExecute = './install-app.sh'}
-   }
-   Set-AzVMExtension @Params
+# Видалити існуючу ресурсну групу, якщо вона є
+$existingRG = Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+if ($existingRG) {
+    Write-Host "Removing existing resource group to avoid duplicate networks..."
+    Remove-AzResourceGroup -Name $ResourceGroupName -Force
+    Start-Sleep -Seconds 60  # Чекати повного видалення
 }
 
-Write-Host "Creating a public IP ..."
-$publicIP = New-AzPublicIpAddress -Name $jumpboxVmName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
-Write-Host "Creating a management VM ..."
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $jumpboxVmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $mngSubnetName `
--VirtualNetworkName $virtualNetworkName `
--SshKeyName $sshKeyName `
--PublicIpAddressName $jumpboxVmName
+# Create Resource Group
+Write-Host "Creating Resource Group..."
+$resourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+Write-Host "Created Resource Group: $ResourceGroupName"
 
+# Create Virtual Network and Subnets (ТІЛЬКИ ОДНА МЕРЕЖА)
+Write-Host "Creating Virtual Network..."
+$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $WebSubnetName -AddressPrefix "10.20.30.0/24"
+$jumpboxSubnet = New-AzVirtualNetworkSubnetConfig -Name $JumpboxSubnetName -AddressPrefix "10.20.31.0/24"
 
-Write-Host "Creating a private DNS zone ..."
-$Zone = New-AzPrivateDnsZone -Name $privateDnsZoneName -ResourceGroupName $resourceGroupName 
-$Link = New-AzPrivateDnsVirtualNetworkLink -ZoneName $privateDnsZoneName -ResourceGroupName $resourceGroupName -Name $Zone.Name -VirtualNetworkId $virtualNetwork.Id -EnableRegistration
+$vnet = New-AzVirtualNetwork `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name $VNetName `
+    -AddressPrefix "10.20.0.0/16" `
+    -Subnet $webSubnet, $jumpboxSubnet
 
+Write-Host "Virtual Network created"
 
-Write-Host "Creating an A DNS record ..."
-$Records = @()
-$Records += New-AzPrivateDnsRecordConfig -IPv4Address $lbIpAddress
-New-AzPrivateDnsRecordSet -Name "todo" -RecordType A -ResourceGroupName $resourceGroupName -TTL 1800 -ZoneName $privateDnsZoneName -PrivateDnsRecords $Records
+# Create Public IP for Jumpbox
+Write-Host "Creating Public IP for Jumpbox..."
+$jumpboxPublicIp = New-AzPublicIpAddress `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name "jumpbox-ip" `
+    -AllocationMethod "Static" `
+    -Sku "Standard"
 
-# Prepare variables, required for creation and configuration of load balancer - 
-# you will need them to setup a load balancer 
-$webSubnetId = (Get-AzVirtualNetworkSubnetConfig -Name $webSubnetName -VirtualNetwork $virtualNetwork).Id
+# Create NSG for Web Subnet
+Write-Host "Creating Network Security Groups..."
+$webNsg = New-AzNetworkSecurityGroup `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name "web-nsg"
 
-# Write your code here -> 
-Write-Host "Creating a load balancer ..."
+$webNsg | Add-AzNetworkSecurityRuleConfig `
+    -Name "allow-http" `
+    -Description "Allow HTTP" `
+    -Access Allow `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 100 `
+    -SourceAddressPrefix * `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 80,8080 | Set-AzNetworkSecurityGroup
 
+# Create NSG for Jumpbox
+$jumpboxNsg = New-AzNetworkSecurityGroup `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name "jumpbox-nsg"
 
-# Write-Host "Adding VMs to the backend pool"
-# $vms = Get-AzVm -ResourceGroupName $resourceGroupName | Where-Object {$_.Name.StartsWith($webVmName)}
-# foreach ($vm in $vms) {
-#    $nic = Get-AzNetworkInterface -ResourceGroupName $resourceGroupName | Where-Object {$_.Id -eq $vm.NetworkProfile.NetworkInterfaces.Id}    
-#    $ipCfg = $nic.IpConfigurations | Where-Object {$_.Primary} 
-#    $ipCfg.LoadBalancerBackendAddressPools.Add($bepool)
-#    Set-AzNetworkInterface -NetworkInterface $nic
-# }
+$jumpboxNsg | Add-AzNetworkSecurityRuleConfig `
+    -Name "allow-ssh" `
+    -Description "Allow SSH" `
+    -Access Allow `
+    -Protocol Tcp `
+    -Direction Inbound `
+    -Priority 100 `
+    -SourceAddressPrefix * `
+    -SourcePortRange * `
+    -DestinationAddressPrefix * `
+    -DestinationPortRange 22 | Set-AzNetworkSecurityGroup
+
+# Create Web Servers
+Write-Host "Creating Web Servers..."
+$securePassword = ConvertTo-SecureString "Azure123456!" -AsPlainText -Force
+$webServerCred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
+
+for ($i = 1; $i -le 2; $i++) {
+    Write-Host "Creating webserver-$i"
+    
+    $nic = New-AzNetworkInterface `
+        -ResourceGroupName $ResourceGroupName `
+        -Location $Location `
+        -Name "webserver-$i-nic" `
+        -Subnet $vnet.Subnets[0] `
+        -NetworkSecurityGroup $webNsg
+    
+    $vm = New-AzVM `
+        -ResourceGroupName $ResourceGroupName `
+        -Location $Location `
+        -Name "webserver-$i" `
+        -Image "Ubuntu2204" `
+        -Size "Standard_B1s" `
+        -Credential $webServerCred `
+        -NetworkInterface $nic `
+        -GenerateSshKey `
+        -SshKeyName $SshKeyName
+    
+    Write-Host "Created webserver-$i"
+}
+
+# Create Jumpbox VM
+Write-Host "Creating Jumpbox VM..."
+$jumpboxNic = New-AzNetworkInterface `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name "jumpbox-nic" `
+    -Subnet $vnet.Subnets[1] `
+    -NetworkSecurityGroup $jumpboxNsg `
+    -PublicIpAddress $jumpboxPublicIp
+
+$jumpboxVm = New-AzVM `
+    -ResourceGroupName $ResourceGroupName `
+    -Location $Location `
+    -Name "jumpbox" `
+    -Image "Ubuntu2204" `
+    -Size "Standard_B1s" `
+    -Credential $webServerCred `
+    -NetworkInterface $jumpboxNic `
+    -GenerateSshKey `
+    -SshKeyName $SshKeyName
+
+Write-Host "Created jumpbox VM"
+
+# Create Private DNS Zone
+Write-Host "Creating Private DNS Zone..."
+$dnsZone = New-AzPrivateDnsZone `
+    -ResourceGroupName $ResourceGroupName `
+    -Name "or.nottodo"
+
+$dnsLink = New-AzPrivateDnsVirtualNetworkLink `
+    -ResourceGroupName $ResourceGroupName `
+    -ZoneName "or.nottodo" `
+    -Name "vnet-link" `
+    -VirtualNetworkId $vnet.Id
+
+# Create DNS record
+$recordConfig = New-AzPrivateDnsRecordConfig -IPv4Address "10.20.30.62"
+New-AzPrivateDnsRecordSet `
+    -ResourceGroupName $ResourceGroupName `
+    -ZoneName "or.nottodo" `
+    -Name "todo" `
+    -RecordType A `
+    -Ttl 3600 `
+    -PrivateDnsRecords $recordConfig
+
+Write-Host "DNS Zone created"
+
+# Create Load Balancer
+Write-Host "Creating Load Balancer..."
+$frontendIP = New-AzLoadBalancerFrontendIpConfig `
+    -Name "lb-frontend" `
+    -PrivateIpAddress "10.20.30.62" `
+    -Subnet $vnet.Subnets[0]
+
+$backendPool = New-AzLoadBalancerBackendAddressPoolConfig -Name "webBackendPool"
+
+$probe = New-AzLoadBalancerProbeConfig `
+    -Name "webHealthProbe" `
+    -Protocol Tcp `
+    -Port 8080 `
+    -IntervalInSeconds 15 `
+    -ProbeCount 2
+
+$lbrule = New-AzLoadBalancerRuleConfig `
+    -Name "webLoadBalancerRule" `
+    -FrontendIpConfiguration $frontendIP `
+    -BackendAddressPool $backendPool `
+    -Probe $probe `
+    -Protocol Tcp `
+    -FrontendPort 80 `
+    -BackendPort 8080
+
+$loadBalancer = New-AzLoadBalancer `
+    -ResourceGroupName $ResourceGroupName `
+    -Name "webLoadBalancer" `
+    -Location $Location `
+    -Sku "Standard" `
+    -FrontendIpConfiguration $frontendIP `
+    -BackendAddressPool $backendPool `
+    -LoadBalancingRule $lbrule `
+    -Probe $probe
+
+Write-Host "Load Balancer created successfully"
+
+# Add VMs to Load Balancer
+Write-Host "Adding VMs to Load Balancer backend pool..."
+$backendPool = Get-AzLoadBalancerBackendAddressPool `
+    -ResourceGroupName $ResourceGroupName `
+    -LoadBalancerName "webLoadBalancer" `
+    -Name "webBackendPool"
+
+for ($i = 1; $i -le 2; $i++) {
+    $nic = Get-AzNetworkInterface -Name "webserver-$i-nic" -ResourceGroupName $ResourceGroupName
+    $nic.IpConfigurations[0].LoadBalancerBackendAddressPools = @($backendPool)
+    Set-AzNetworkInterface -NetworkInterface $nic
+    Write-Host "Added webserver-$i to backend pool"
+}
+
+Write-Host "Deployment completed successfully!"
